a9148db9aa4c246113e3623197f0ac65fbae74f4
AliHussain <alihussain08950@gmail.com>
2024-09-03 16:23:44 +0300

    Implementing route protection causes unstability and bugs within the code

--- main_app/views.py
+++ main_app/views.py
@@ -8,77 +8,14 @@ from django.contrib.auth.views import LoginView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class Home(LoginView):
     template_name = 'home.html'
 
 def about(request):
     return render(request, 'about.html')
-
-def car_index(request):
-    cars = Car.objects.filter(user=request.user)
-    return render(request, 'cars/index.html', {'cars': cars})
-
-def car_detail(request, car_id):
-    car = Car.objects.get(id=car_id)
-    return render(request, 'cars/details.html', {'car': car})
-
-class CarCreate(CreateView):
-    model = Car
-    fields = '__all__'
-    success_url = '/cars/'
-    def form_valid(self, form):
-        form.instance.user = self.request.user  
-        return super().form_valid(form)
-
-class CarUpdate(UpdateView):
-    model = Car
-    fields = ['make', 'model', 'description', 'year']
-
-class CarDelete(DeleteView):
-    model = Car
-    success_url = '/cars/'
-
-def car_detail(request, car_id):
-    car = Car.objects.get(id=car_id)
-    cleaning_form = CleaningForm()
-    return render(request, 'cars/details.html', {
-        'car': car, 'cleaning_form': cleaning_form
-    })
-
-def add_cleaning(request, car_id):
-    form = CleaningForm(request.POST)
-    if form.is_valid():
-        new_cleaning = form.save(commit=False)
-        new_cleaning.car_id = car_id
-        new_cleaning.save()
-    return redirect('car-detail', car_id=car_id)
-
-class CleaningUpdate(UpdateView):
-    model = Cleaning
-    fields = ['date', 'time']
-    template_name = 'cleanings/update.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['car_id'] = self.object.car.id
-        return context
-
-    def get_success_url(self):
-        return reverse('car-detail', args=[self.object.car.id])
-
-class CleaningDelete(DeleteView):
-    model = Cleaning
-    template_name = 'cleanings/delete_confirm.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['car_id'] = self.object.car.id
-        return context
-
-    def get_success_url(self):
-        return reverse('car-detail', args=[self.object.car.id])
 
 def signup(request):
     error_message = ''
@@ -93,4 +30,67 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'signup.html', context)
+
+@login_required
+def car_index(request):
+    cars = Car.objects.filter(user=request.user)
+    return render(request, 'cars/index.html', {'cars': cars})
+
+class CarCreate(LoginRequiredMixin, CreateView):
+    model = Car
+    fields = '__all__'
+    success_url = '/cars/'
+    def form_valid(self, form):
+        form.instance.user = self.request.user  
+        return super().form_valid(form)
+
+class CarUpdate(LoginRequiredMixin, UpdateView):
+    model = Car
+    fields = ['make', 'model', 'description', 'year']
+
+class CarDelete(LoginRequiredMixin, DeleteView):
+    model = Car
+    success_url = '/cars/'
+
+@login_required
+def car_detail(request, car_id):
+    car = Car.objects.get(id=car_id)
+    cleaning_form = CleaningForm()
+    return render(request, 'cars/details.html', {
+        'car': car, 'cleaning_form': cleaning_form
+    })
+
+@login_required
+def add_cleaning(request, car_id):
+    form = CleaningForm(request.POST)
+    if form.is_valid():
+        new_cleaning = form.save(commit=False)
+        new_cleaning.car_id = car_id
+        new_cleaning.save()
+    return redirect('car-detail', car_id=car_id)
+
+class CleaningUpdate(LoginRequiredMixin, UpdateView):
+    model = Cleaning
+    fields = ['date', 'time']
+    template_name = 'cleanings/update.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['car_id'] = self.object.car.id
+        return context
+
+    def get_success_url(self):
+        return reverse('car-detail', args=[self.object.car.id])
+
+class CleaningDelete(LoginRequiredMixin, DeleteView):
+    model = Cleaning
+    template_name = 'cleanings/delete_confirm.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['car_id'] = self.object.car.id
+        return context
+
+    def get_success_url(self):
+        return reverse('car-detail', args=[self.object.car.id])
    
